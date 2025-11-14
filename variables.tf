84a04b0a932a6cad34ca89ae654515fa0d6d9e50
tonydwilliams101 <tonydwilliams101@gmail.com>
2025-11-14 18:16:22 +0000

    removed git vars from root terraform code

--- variables.tf
+++ variables.tf
@@ -4,12 +4,12 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "github_user" {
-  description = "git hub user to run pipeline"
-  type = string
-}
+# variable "github_user" {
+#   description = "git hub user to run pipeline"
+#   type = string
+# }
 
-variable "github_repo" {
-  description = "github repository to run pipeline."
-  type = string
-}
+# variable "github_repo" {
+#   description = "github repository to run pipeline."
+#   type = string
+# }
